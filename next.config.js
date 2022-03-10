f167f03cca0e3f2073bd008adfdee1fb55b0372c
thatviper <challanaganarasimhareddy@gmail.com>
2022-03-11 01:06:33 +0530

    removed env vars

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 module.exports = () => {
   return {
     reactStrictMode: true,
-    env: {
-      PUBLIC_SUPABASE_ANON_KEY:
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhycGZ2dGxkb292bGRpZ2tjaWtsIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDU5NDI5NTgsImV4cCI6MTk2MTUxODk1OH0.ThVqM7_IfzpWdwoyPKqcBIA3fnxiBck0CJ35N7qfNLM",
-      SECRET_KEY:
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhycGZ2dGxkb292bGRpZ2tjaWtsIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY0NTk0Mjk1OCwiZXhwIjoxOTYxNTE4OTU4fQ.40Dh9bPu_JywpzJeCsS7dSYm9yPZvLvPXtg4_j0E5-U",
-      PROJECT_SUPABASE_URL: "https://xrpfvtldoovldigkcikl.supabase.co",
-      JWT_SECRET: "1ad17e1e-58c0-4d3f-960c-b6f9f14a5c9c",
-    },
+    env: {},
   };
 };
